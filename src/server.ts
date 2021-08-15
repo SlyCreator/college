352ecd2c1d24a5467babe7dd3a248564884ea73b
slycreator <chinonso1amaechi@gmial.com>
2021-08-15 12:47:01 +0100

    server.ts update

---
+++ src/server.ts
@@ -0,0 +1,36 @@
+import {ApolloServer} from "apollo-server-express";
+import * as Express from "express";
+import "reflect-metadata"
+import {buildSchema} from "type-graphql";
+import * as Mongoose from "mongoose";
+
+async function startServer() {
+    require("dotenv").config(__dirname + ".env")
+    const schema = await buildSchema({
+        resolvers:[],
+        emitSchemaFile:true
+    })
+    const app = Express();
+
+    const MONGO_USER = process.env.MONGODB_USER
+    const MONGO_PASS = process.env.MONGODB_PASS
+    Mongoose.connect(
+        `mongodb+srv://${MONGO_USER}:${MONGO_PASS}@cluster0-xv4mh.mongodb.net/test?retryWrites=true&w=majority`,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    ).then(() =>{
+        console.log("Mongo server connected sucessfully")
+
+        const server = new ApolloServer({schema,context:()=>({}),})
+        server.applyMiddleware({app})
+        const  PORT = process.env.PORT;
+        app.listen(PORT,()=>{
+            console.log(`server is running at port:${PORT}`)
+        })
+    })
+    .catch(err =>{
+        console.log(err)
+    })
+}
+startServer();
